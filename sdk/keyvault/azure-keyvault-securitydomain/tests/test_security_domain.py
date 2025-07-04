add73fe2381f900e3bc36cb1b8825d38ded5eeac
mccoyp <mcpatino@microsoft.com>
2025-07-04 02:10:48 +0000

    wip; Add utilities for locally wrapping security domain

--- sdk/keyvault/azure-keyvault-securitydomain/tests/test_security_domain.py
+++ sdk/keyvault/azure-keyvault-securitydomain/tests/test_security_domain.py
@@ -11,7 +11,7 @@ from azure.keyvault.securitydomain import SecurityDomainClient
 
 from _shared.test_case import KeyVaultTestCase
 from _test_case import ClientPreparer
-from utils import get_certificate_info, write_security_domain, write_transfer_key
+from utils import get_certificate_info, use_wrapping_keys, write_security_domain, write_transfer_key
 
 
 class TestSecurityDomain(KeyVaultTestCase):
@@ -35,10 +35,11 @@ class TestSecurityDomain(KeyVaultTestCase):
         transfer_key = json.loads(jwk)
         write_transfer_key(transfer_key)
         write_security_domain(result.value)
+        wrapped_security_domain = use_wrapping_keys()
 
         # At this point, use the Azure CLI to encrypt the security domain to prepare for upload
         # `az keyvault security-domain restore-blob --sd-exchange-key <>-transfer-key.pem --sd-file <>-security-domain.json --sd-wrapping-keys <>-certificate0.key <>-certificate1.key <>-certificate2.key --sd-file-restore-blob <>-restore-blob.json`
-        poller = upload_client.begin_upload(security_domain=result)
+        poller = upload_client.begin_upload(security_domain=wrapped_security_domain)
         result = poller.result()
         assert result is None
         status = upload_client.get_upload_status()
