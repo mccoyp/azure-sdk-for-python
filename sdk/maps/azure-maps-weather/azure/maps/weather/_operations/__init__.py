3e3ca7596ed73c9ded2c7779a6de6589b04b31d0
mccoyp <mcpatino@microsoft.com>
2026-03-14 05:42:24 +0000

    Generate Weather with customizations

--- sdk/maps/azure-maps-weather/azure/maps/weather/_operations/__init__.py
+++ sdk/maps/azure-maps-weather/azure/maps/weather/_operations/__init__.py
@@ -12,21 +12,12 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ._patch import *  # pylint: disable=unused-wildcard-import
 
-from ._client import MapsWeatherClient  # type: ignore
-from ._version import VERSION
+from ._operations import _MapsWeatherClientOperationsMixin  # type: ignore # pylint: disable=unused-import
 
-__version__ = VERSION
-
-try:
-    from ._patch import __all__ as _patch_all
-    from ._patch import *
-except ImportError:
-    _patch_all = []
+from ._patch import __all__ as _patch_all
+from ._patch import *
 from ._patch import patch_sdk as _patch_sdk
 
-__all__ = [
-    "MapsWeatherClient",
-]
+__all__ = []
 __all__.extend([p for p in _patch_all if p not in __all__])  # pyright: ignore
-
 _patch_sdk()
